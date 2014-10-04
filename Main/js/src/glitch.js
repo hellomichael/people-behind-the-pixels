fd26afd7fe6d1a4600f6c835caa7e6f4f7520f84
Michael Ngo <michael@hellomichael.com>
2014-10-05 04:24:16 +1100

    Randomized glitch, animateIn/Out, canvas resizing

--- Main/js/src/glitch.js
+++ Main/js/src/glitch.js
@@ -1,3 +1,58 @@
+var Artifact = function (glitch, blend, width, height, x, y, targetX, targetY) {
+    this.glitch = glitch;
+
+    this.blend = blend;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.targetX = targetX;
+    this.targetY = targetY;
+}
+
+Artifact.prototype = {
+    constructor: Artifact,
+
+    blendMode: function(blend) {
+        this.blend = blend;
+    },
+
+    move: function(offsetX, offsetY) {
+        if (this.blend === 'offset') {
+            this.targetX += offsetX;
+            this.targetY += offsetY;
+        }
+
+        else {
+            this.x += offsetX;
+            this.y += offsetY;
+        }
+    },
+
+    scale: function(scaleX, scaleY) {
+        this.width *= scaleX;
+        this.height *= scaleY;
+    },
+
+    draw: function() {
+       if (this.blend === 'add') {
+           this.glitch.context.fillStyle = 'rgba(255,255,255,0.75)';
+           this.glitch.context.fillRect(this.glitch.x + this.x, this.glitch.y + this.y, this.width, this.height);
+       }
+
+       else if (this.blend === 'subtract') {
+           this.glitch.context.clearRect(this.glitch.x + this.x, this.glitch.y + this.y, this.width, this.height);
+       }
+
+       else if (this.blend === 'offset') {
+           this.clone = this.glitch.context.getImageData(this.glitch.x + this.x, this.glitch.y + this.y, this.width, this.height);
+           this.glitch.context.clearRect(this.glitch.x + this.x, this.glitch.y + this.y, this.width, this.height);
+           this.glitch.context.putImageData(this.clone, this.glitch.x + this.x + this.targetX, this.glitch.y + this.y + this.targetY);
+       }
+    }
+}
+
 /**
 * Creates new scene
 * @param {name}
@@ -6,45 +61,53 @@
 var Glitch = function(speaker, x, y) {
     this.speaker = speaker;
 
+    // 2 types of glitches
+    this.direction = Math.random() < 0.5 ? 'left' : 'right';
+
+    // Get the canvas
     this.canvas = document.getElementById('speakers');
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
 
+    // Type Settings
     this.context = this.canvas.getContext('2d');
     this.context.fillStyle = 'white';
     this.context.font = 'bold 25px "Futura"';
-    this.context.textBaseline = 'hanging';
-    this.padding = 25;
-    this.count = 0;
-    this.currentFrame;
+    this.context.textBaseline = 'middle';
 
+    // Type dimensions
     this.metrics = this.context.measureText(this.speaker);
-
-    this.x = this.canvas.width/2 + x;
-    this.y = this.canvas.height/2 + y;
-
     this.width = Math.round(this.metrics.width);
-    this.height = 20; //Unfortunately hardcoded
+    this.height = 23;
 
-    this.context.fillText(this.speaker, 0, 0);
+    this.x = this.canvas.width/2 - this.width/2 + x;
+    this.y = this.canvas.height/2 - this.height/2 + y;
 
-    // Create clone
+    // Draw type onto canvas
+    this.context.fillText(this.speaker, 0, 12.5);
+
+    // Clone canvas and draw text at center
     this.clone = this.context.getImageData(0, 0, this.width, this.height);
-
-    // Create artifact
-    this.artifactHeight;
-    this.artifactOffsetX;
-    this.artifactOffsetY;
-    this.artifact = this.context.getImageData(this.width/2, this.height-7, this.width/2, 7);
-
-    // Clear canvas before redrawing
     this.canvas.width = this.canvas.width;
+
+    var context = this;
+    window.addEventListener('resize', function() {context.onResize(x, y);});
 };
 
 Glitch.prototype = {
     constructor: Glitch,
 
-    play: function() {
+    onResize: function (x, y) {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        this.x = this.canvas.width/2 - this.width/2 + x;
+        this.y = this.canvas.height/2 - this.height/2 + y;
+
+        this.goToFrame(this.currentFrame);
+    },
+
+    animateIn: function() {
         var that = this;
 
         new TWEEN.Tween({frame: 1})
@@ -52,45 +115,138 @@ Glitch.prototype = {
             .onUpdate(function (e) {
                 that.currentFrame = Math.round(this.frame);
 
-                that.nextFrame(that.currentFrame);
+                if (that.previousFrame != that.currentFrame) {
+                    that.goToFrame(that.currentFrame);
+                }
+
+                that.previousFrame = that.currentFrame;
             })
         .start();
     },
 
-    nextFrame: function(currentFrame) {
-        if (currentFrame === 1) {
-            // Remove artifact
-            this.canvas.width = this.canvas.width;
+    animateOut: function() {
+        var that = this;
 
-            this.artifactHeight = 7;
+        new TWEEN.Tween({frame: 7})
+            .to({frame: 0}, 750)
+            .onUpdate(function (e) {
+                that.currentFrame = Math.round(this.frame);
+
+                if (that.previousFrame != that.currentFrame) {
+                    that.goToFrame(that.currentFrame);
+                }
+
+                that.previousFrame = that.currentFrame;
+            })
+        .start();
+    },
+
+    goToFrame: function(currentFrame) {
+        // Clear the frame
+        this.canvas.width = this.canvas.width;
+
+        if (currentFrame === 1) {
             this.context.putImageData(this.clone, this.x, this.y);
-            this.context.clearRect(this.x + this.width/2, this.y + this.height - this.artifactHeight, this.width/2, this.artifactHeight);
         }
 
         else if (currentFrame === 2) {
-            // Move artifact
-            this.canvas.width = this.canvas.width;
-            this.artifactOffsetX = this.width/3;
-            this.artifactOffsetY = 3;
-
             this.context.putImageData(this.clone, this.x, this.y);
-            this.context.clearRect(this.x + this.width/2, this.y + this.height - this.artifactHeight, this.width/2, this.artifactHeight);
-            this.context.putImageData(this.artifact, this.x + this.width/2 + this.artifactOffsetX, this.y + this.height-this.artifactHeight + this.artifactOffsetY);
+            var offset = Math.random() * this.width;
+
+            if (this.direction === 'left') {
+
+                this.artifact1 = new Artifact(this, 'add', 27, 20, offset, -13);
+                this.artifact2 = new Artifact(this, 'add', 27, 20, offset, -13);
+                this.artifact3 = new Artifact(this, 'add', 27, 20, offset, -13);
+
+                this.artifact4 = new Artifact(this, 'offset', this.width/1.5, 6, this.width/3, this.height - 6, 5, 8);
+                this.artifact5 = new Artifact(this, 'subtract', this.width/1.5, 6, this.width/3, this.height - 6);
+                this.artifact6 = new Artifact(this, 'add', this.width/1.5, 6, this.width/3, this.height - 6);
+
+                this.artifact2.move(32, 0);
+                this.artifact2.scale(1, 0.3);
+
+                this.artifact3.move(-5, 0);
+                this.artifact3.scale(1.2, 1.2);
+
+                this.artifact6.move(0, 20);
+                this.artifact6.scale(1, 0.6);
+            }
+
+            else {
+                this.artifact1 = new Artifact(this, 'add', 27, 20, this.width - offset, -13);
+                this.artifact2 = new Artifact(this, 'add', 27, 20, this.width - offset, -13);
+                this.artifact3 = new Artifact(this, 'add', 27, 20, this.width - offset, -13);
+
+                this.artifact4 = new Artifact(this, 'offset', this.width/1.5, 6, -1, this.height - 6, -5, 8);
+                this.artifact5 = new Artifact(this, 'subtract', this.width/1.5, 6, -1, this.height - 6);
+                this.artifact6 = new Artifact(this, 'add', this.width/1.5, 6, -1, this.height - 6);
+
+                this.artifact2.move(-32, 0);
+                this.artifact2.scale(1, 0.3);
+
+                this.artifact3.move(0, 0);
+                this.artifact3.scale(1.2, 1.2);
+
+                this.artifact6.move(0, 20);
+                this.artifact6.scale(1, 0.6);
+            }
+
+            this.artifact1.draw();
+            this.artifact2.draw();
+            this.artifact5.draw();
+            this.artifact6.draw();
         }
 
         else if (currentFrame === 3) {
-            // Move artifact
-            this.canvas.width = this.canvas.width;
-            this.artifactOffsetX = 7;
-
             this.context.putImageData(this.clone, this.x, this.y);
-            this.context.clearRect(this.width/2, this.height - this.artifactHeight, this.width/2, this.artifactHeight);
-            this.context.putImageData(this.artifact, this.x + this.width/2 + this.artifactOffsetX, this.y + this.height-this.artifactHeight + this.artifactOffsetY);
+
+            this.artifact1.draw();
+            this.artifact2.draw();
+            this.artifact3.draw();
+            this.artifact5.draw();
+            this.artifact6.draw();
         }
 
         else if (currentFrame === 4) {
-            this.canvas.width = this.canvas.width;
+            this.context.putImageData(this.clone, this.x, this.y);
+
+            this.artifact4.draw();
+        }
+
+        else if (currentFrame === 5) {
+            this.context.putImageData(this.clone, this.x, this.y);
+
+            if (this.direction === 'left') {
+                this.artifact1.scale(1, 0.3);
+                this.artifact1.move(10, 23);
+                this.artifact2.move(-28, 20);
+                this.artifact4.move(10, 0);
+            }
+
+            else {
+                this.artifact1.scale(1, 0.3);
+                this.artifact1.move(10, 23);
+                this.artifact2.move(28, 20);
+                this.artifact4.move(-10, 0);
+            }
+
+            this.artifact1.draw();
+            this.artifact2.draw();
+            this.artifact4.draw();
+        }
+
+        else if (currentFrame === 6) {
+            this.context.putImageData(this.clone, this.x, this.y);
+
+            this.artifact2.draw();
+            this.artifact4.draw();
+        }
+
+        else if (currentFrame === 7) {
             this.context.putImageData(this.clone, this.x, this.y);
         }
+
+        this.context.fill();
     }
 };
